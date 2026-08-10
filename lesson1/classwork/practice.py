e4fad855954c76c6d28d298983c45c8f1cf12af4
AlexCanTheMatrix <alexbcan14@gmail.com>
2026-08-10 20:45:59 +0000

    work

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
-
+My_favorite_number = 7
+print('My favorite number is', My_favorite_number)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
+myheight= 1.52
+print('My height is', myheight)
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+myfavoriteanimal= 'a cat'
+print('My favorite animal is', myfavoriteanimal )
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
+firstname = 'Alex'
+lastname = 'Can'
+print('My first name is', firstname,'my last name is', lastname )
 
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+firstname = 'Alex'
+age = 12
+color = 'red'
+print('My first name is', firstname,'my favorite color is', color,'my age is', age)
