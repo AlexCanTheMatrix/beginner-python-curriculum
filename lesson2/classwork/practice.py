9ff3dbc9fdbce42ab456907d01ce2639af84af42
AlexCanTheMatrix <alexbcan14@gmail.com>
2026-08-11 21:07:48 +0000

    Co-authored-by: Toby Hansen <tethys2@users.noreply.github.com>

--- lesson2/classwork/practice.py
+++ lesson2/classwork/practice.py
@@ -1,25 +1,28 @@
 # Problem 1
 # Ask the user for their age.
 # Calculate and print how many decades old they are, rounded to the nearest whole number.
-
-
+age = input("How old are you")
+print("You are ", round(int(age)/10), " decades old")
 
 # Problem 2
 # Ask the user to enter a number.
 # Print the result of multiplying that number by 5.
-
+number= input("Pick a random number")
+print(int(number)*5)
 
 
 # Problem 3
 # Use a for loop to print "I will learn Python!" 3 times.
-
-
+for x in range(3):
+    print("I will learn python")
 
 # Problem 4
 # Ask the user for their name and age.
 # Print their name and how old they will be one year in a single sentence.
-
-
+age = input("how old are you")
+print("You are going to be", int(age)+1, "next year")
 
 # Problem 5
 # Use a for loop to print the numbers from 2 to 8, one per line.
+for y in range(2,9):
+    print(y)
